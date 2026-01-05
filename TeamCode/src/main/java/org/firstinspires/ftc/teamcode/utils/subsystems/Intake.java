7f574122e3f5cd978510cc71e69bb8ccd028de73
Gluons5439 <mhsgluons1@gmail.com>
2026-01-04 22:15:43 -0600

    new code ;(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/subsystems/Intake.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.utils.subsystems;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.hardware.motors.Motor;
-
+/*
 public class Intake extends SubsystemBase {
     public Motor intake;
 
@@ -33,5 +33,61 @@ public class Intake extends SubsystemBase {
             intake.set(power);
         }
 
+    }
+}
+
+*/
+
+
+public class Intake extends SubsystemBase {
+
+    public Motor intake;
+
+    // Same behavior as your old intake
+    private static final double INTAKE_POWER = -1.0;
+    private static final double OUTTAKE_POWER = 0.25;
+
+    // Internal state
+    private double minPower = 0.0;
+    private double power = 0.0;
+
+    public Intake(HardwareMap hMap) {
+        intake = new Motor(hMap, "Intake");
+
+        intake.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        intake.setInverted(true); // matches old negative power logic
+        intake.set(minPower);
+    }
+
+
+    public void setControl(boolean intakeIn, boolean intakeOut) {
+        if (intakeIn) {
+            power = INTAKE_POWER;
+        } else if (intakeOut) {
+            power = OUTTAKE_POWER;
+        } else {
+            power = 0.0;
+        }
+    }
+
+
+    public void forceIntake() {
+        power = INTAKE_POWER;
+    }
+
+    public void setMinPower(double minPower) {
+        this.minPower = minPower;
+    }
+
+
+    @Override
+    public void periodic() {
+        double pow = Math.max(Math.abs(power), Math.abs(minPower));
+
+        if (pow == Math.abs(minPower)) {
+            intake.set(minPower);
+        } else {
+            intake.set(power);
+        }
     }
 }
